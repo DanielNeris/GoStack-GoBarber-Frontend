69746dd9a64924069aef55e4b765d28d507581bf
Daniel de Souza Neris <danielneris01@gmail.com>
2020-04-26 15:23:02 -0300

    Página de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import SingIn from './pages/SignIn';
+
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <h1>teste</h1>
+    <SingIn />
 
     <GlobalStyle />
   </>
