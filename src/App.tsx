f5d4467ab5ed3925c5489d3be81e17791a64ceab
Daniel de Souza Neris <danielneris01@gmail.com>
2020-04-26 16:05:57 -0300

    Página de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import SingIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SingIn />
+    {/* <SingIn /> */}
+    <SignUp />
 
     <GlobalStyle />
   </>
